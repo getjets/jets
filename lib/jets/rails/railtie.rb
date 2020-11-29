b71a81db27a0b8e5ed5262447000eb6986ca8112
Lauri Jutila <git@laurijutila.com>
2020-11-30 00:12:55 +0200

    Adds gem generator to Railtie.

--- lib/jets/rails/railtie.rb
+++ lib/jets/rails/railtie.rb
@@ -10,6 +10,7 @@ module Jets
     class Railtie < ::Rails::Railtie
       generators do
         require "generators/jets/engine/engine_generator"
+        require "generators/jets/gem/gem_generator"
         require "generators/jets/install/install_generator"
       end
     end
